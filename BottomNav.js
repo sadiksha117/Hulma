d007c382c2773780a03dbded60034294640ca266
sadiksha117 <84380818+sadiksha117@users.noreply.github.com>
2022-12-02 15:13:06 +0545

    Add files via upload

---
+++ BottomNav.js
@@ -0,0 +1,82 @@
+import {View,Text, StyleSheet} from "react-native";
+import React from "react";  
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import HomeScreen from  '../Screens/HomeScreen';
+import {Center,Pressable} from "native-base";
+import {Entypo, AntDesign, FontAwesome , FontAwesome5,MaterialCommunityIcons} from "@expo/vector-icons";
+import Colors from '../color';
+
+const Tab = createBottomTabNavigator ()
+const CustomTab =({children,onPress}) =>(
+    <Pressable onPress={onPress} 
+    h={70} 
+    w={70}
+   
+     rounded='full'
+      bg={Colors.main} 
+      top={-30}
+       shadow={2}>\
+    {children}
+    </Pressable>
+)
+const BottomNav = () => {
+    return (
+        <Tab.Navigator backBehavior="main" initialRouteName="Main" screenOptions={{
+            tabBarShowLabel:false,
+            tabBarStyle:{...styles.tab},
+            headerShown:false,
+            tabBarHideOnKeyboard:true
+        }}>
+            <Tab.Screen name="Main" component={HomeScreen} options={{
+                tabBarIcon:({focused}) => (
+                    <Center>
+                        {focused ? (
+                            <Entypo name="home" size={24} color={Colors.white}/>
+                        ):(
+                            <AntDesign name="home" size={24} color={Colors.white}/>
+                        )}
+                    </Center>
+                )
+            }}/>
+            {/* cart */}
+            <Tab.Screen name="Cart" component={CartScreen} options={{
+                tabBarrButton:(props) => <CustomTab{...props}/>,
+                tabBarIcon:({focused}) => (
+                    <Center>
+                        {focused ? (
+                            <FontAwesome5 name="shopping-basket" size={24} color={Colors.main}/>
+                        ):(
+                            <MaterialCommunityIcons name="shopping-outline" size={24} color={Colors.black}/>
+                        )}
+                    </Center>
+                )
+            }}/>
+            {/* profile */}
+            <Tab.Screen name="Profile" component={ProfileScreen} options={{
+                tabBarIcon:({focused}) => (
+                    <Center>
+                        {focused ? (
+                            <FontAwesome name="user" size={24} color={Colors.main}/>
+                        ):(
+                            <AntDesign name="user" size={24} color={Colors.black}/>
+                        )}
+                    </Center>
+                )
+            }}/>
+
+        </Tab.Navigator>
+    );
+};
+ const styles= StyleSheet.created({
+    tab: {
+        elevation:0,
+        backgroundColor:Colors.white,
+        height: 60,
+       
+
+    },
+ });
+
+
+
+export default BottomNav;
