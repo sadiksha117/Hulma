2e6fde2d983b5704a1cb57f07e05a894eacbf7db
sadiksha117 <84380818+sadiksha117@users.noreply.github.com>
2022-12-02 15:12:19 +0545

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-status";
+import {NativeBaseProvider,StatusBar} from "native-base";
+import LoginScreen from "./src/Screens/LoginScreen";
+import RegisterScreen from "./src/Screens/RegisterScreen";
+import OrderScreen from "./src/Screens/OrderScreen";
+import BottomNav from "./src/Screens/BottomNav";
 
-import { NativeBaseProvider } from "native-base"; 
-
-import HomeScreen from './src/Screens/HomeScreen'
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      < HomeScreen />
-    </NativeBaseProvider>
-  );
+    return(
+        <NativeBaseProvider>
+            <NavigationContainer>
+                <StatusBar hidden={true} />
+                <Stack.Navigator initialRouteName="Bottom"
+                screenOption={{
+                    headerShown: false,
+                }}
+                >
+                    <Stack.screen name="Login" component={LoginScreen} />
+                    <Stack.screen name="Register" component={RegisterScreen} />
+                    <Stack.screen name="Order" component={OrderScreen} />
+                    <Stack.screen name="Bottom" component={BottomNav} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </NativeBaseProvider>
+    );
 }
+
